cc32748daee9303654bc41318bd25f28a1460afe
mahfuj890 <mahfujali490@gmail.com>
2023-05-01 23:12:02 +0600

    show todo and some operation

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,15 @@
+import { useSelector } from "react-redux";
 import Todo from "./Todo";
 
 export default function TodoList() {
+  const getTodos = useSelector((state) => state.todos);
+ 
+
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-      <Todo />
+      {getTodos.map((item) => {
+        return <Todo todo={item} key={item?.id} />;
+      })}
     </div>
   );
 }
