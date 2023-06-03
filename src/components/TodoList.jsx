e66e0129f336b519507ed451fd1f7b23633bc5a3
mahfuj890 <mahfujali490@gmail.com>
2023-06-03 23:15:35 +0600

    filter fnctionality

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,26 +5,30 @@ export default function TodoList() {
   const getTodos = useSelector((state) => state.todos);
   const getFilter = useSelector((state) => state.filter);
 
+  const filterByStatus = (todo) => {
+    switch (getFilter?.status) {
+      case "complete":
+        return todo?.complete;
+      case "incomplete":
+        return !todo?.complete;
+
+      default:
+        return true;
+    }
+  };
+
+  const filterByColor = (colorItem) => {
+    if (getFilter?.colors?.length > 0) {
+      return getFilter?.colors.includes(colorItem?.color);
+    }
+    return true;
+  };
+
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
       {getTodos
-        .filter((todo) => {
-          switch (getFilter?.status) {
-            case "complete":
-              return todo?.complete;
-            case "incomplete":
-              return !todo?.complete;
-
-            default:
-              return true;
-          }
-        })
-        .filter((colorItem) => {
-          if (getFilter?.colors?.length > 0) {
-            return getFilter?.colors.includes(colorItem?.color);
-          }
-          return true;
-        })
+        .filter(filterByStatus)
+        .filter(filterByColor)
         .map((item) => {
           return <Todo todo={item} key={item?.id} />;
         })}
